9a33a86e976905c852dda6d0bcdcc4e7f88e591d
Vladislav <tookcreed@gmail.com>
2013-02-19 02:04:07 +0400

    Add pic Hunter

--- Panzer/HunterImg.cs
+++ Panzer/HunterImg.cs
@@ -2,10 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace Panzer
 {
     class HunterImg
     {
+        Image[] imgRight = new Image[] 
+        {
+            Properties.Resources.HunterRight__1_
+                , Properties.Resources.HunterRight__1_
+                , Properties.Resources.HunterRight__2_
+                , Properties.Resources.HunterRight__2_
+        };
+        Image[] imgUp = new Image[]
+        {
+            Properties.Resources.HunterUp__1_
+                , Properties.Resources.HunterUp__1_
+                , Properties.Resources.HunterUp__2_
+                , Properties.Resources.HunterUp__2_
+        };
+        Image[] imgLeft = new Image[]
+        {
+            Properties.Resources.HunterLeft__1_
+                , Properties.Resources.HunterLeft__1_
+                , Properties.Resources.HunterLeft__2_
+                , Properties.Resources.HunterLeft__2_
+        };
+        Image[] imgDown = new Image[]
+        {
+            Properties.Resources.HunterDown__1_
+                , Properties.Resources.HunterDown__1_
+                , Properties.Resources.HunterDown__2_
+                , Properties.Resources.HunterDown__2_
+        };
+
+        public Image[] ImgRight
+        {
+            get
+            {
+                return imgRight;
+            }
+        }
+        public Image[] ImgUp
+        {
+            get
+            {
+                return imgUp;
+            }
+        }
+        public Image[] ImgLeft
+        {
+            get
+            {
+                return imgLeft;
+            }
+        }
+        public Image[] ImgDown
+        {
+            get
+            {
+                return imgDown;
+            }
+        }
     }
 }
