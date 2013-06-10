71889e902dce5c00b2f87341483df6baf1f5fdf8
Vladislav <tookcreed@gmail.com>
2013-06-10 19:54:21 +0400

    low changes

--- Bilds_Fiz/Bilds_Fiz/Forms/Document.Designer.cs
+++ Bilds_Fiz/Bilds_Fiz/Forms/Document.Designer.cs
@@ -33,20 +33,18 @@
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.печатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.видToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.шаблоныToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.форматированиеТекстаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.цветТекстаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.цветВыделенияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.выравниваниеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.rtxtDoc = new System.Windows.Forms.RichTextBox();
-            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
-            this.colorDialog = new System.Windows.Forms.ColorDialog();
             this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
-            this.fontDialog = new System.Windows.Forms.FontDialog();
             this.шрифтToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rtxtDoc = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.fontDialog = new System.Windows.Forms.FontDialog();
             this.menuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,7 +53,6 @@
             this.menuStrip.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.файлToolStripMenuItem,
-            this.видToolStripMenuItem,
             this.форматированиеТекстаToolStripMenuItem});
             this.menuStrip.Location = new System.Drawing.Point(0, 614);
             this.menuStrip.Name = "menuStrip";
@@ -69,35 +66,21 @@
             this.сохранитьToolStripMenuItem,
             this.печатьToolStripMenuItem});
             this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-            this.файлToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
             this.файлToolStripMenuItem.Text = "Файл";
             // 
             // сохранитьToolStripMenuItem
             // 
             this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
-            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
             this.сохранитьToolStripMenuItem.Text = "Сохранить";
             this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
             // 
             // печатьToolStripMenuItem
             // 
             this.печатьToolStripMenuItem.Name = "печатьToolStripMenuItem";
-            this.печатьToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.печатьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
             this.печатьToolStripMenuItem.Text = "Печать";
-            // 
-            // видToolStripMenuItem
-            // 
-            this.видToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.шаблоныToolStripMenuItem});
-            this.видToolStripMenuItem.Name = "видToolStripMenuItem";
-            this.видToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
-            this.видToolStripMenuItem.Text = "Вид";
-            // 
-            // шаблоныToolStripMenuItem
-            // 
-            this.шаблоныToolStripMenuItem.Name = "шаблоныToolStripMenuItem";
-            this.шаблоныToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
-            this.шаблоныToolStripMenuItem.Text = "Шаблоны";
             // 
             // форматированиеТекстаToolStripMenuItem
             // 
@@ -107,20 +90,20 @@
             this.выравниваниеToolStripMenuItem,
             this.шрифтToolStripMenuItem});
             this.форматированиеТекстаToolStripMenuItem.Name = "форматированиеТекстаToolStripMenuItem";
-            this.форматированиеТекстаToolStripMenuItem.Size = new System.Drawing.Size(143, 20);
+            this.форматированиеТекстаToolStripMenuItem.Size = new System.Drawing.Size(152, 20);
             this.форматированиеТекстаToolStripMenuItem.Text = "Форматирование текста";
             // 
             // цветТекстаToolStripMenuItem
             // 
             this.цветТекстаToolStripMenuItem.Name = "цветТекстаToolStripMenuItem";
-            this.цветТекстаToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.цветТекстаToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
             this.цветТекстаToolStripMenuItem.Text = "Цвет текста";
             this.цветТекстаToolStripMenuItem.Click += new System.EventHandler(this.цветТекстаToolStripMenuItem_Click);
             // 
             // цветВыделенияToolStripMenuItem
             // 
             this.цветВыделенияToolStripMenuItem.Name = "цветВыделенияToolStripMenuItem";
-            this.цветВыделенияToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.цветВыделенияToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
             this.цветВыделенияToolStripMenuItem.Text = "Цвет выделения";
             this.цветВыделенияToolStripMenuItem.Click += new System.EventHandler(this.цветВыделенияToolStripMenuItem_Click);
             // 
@@ -131,8 +114,39 @@
             this.toolStripMenuItem3,
             this.toolStripMenuItem4});
             this.выравниваниеToolStripMenuItem.Name = "выравниваниеToolStripMenuItem";
-            this.выравниваниеToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.выравниваниеToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
             this.выравниваниеToolStripMenuItem.Text = "Выравнивание";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(113, 22);
+            this.toolStripMenuItem2.Text = "<-";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(113, 22);
+            this.toolStripMenuItem3.Text = "<- ->";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(113, 22);
+            this.toolStripMenuItem4.Text = "->";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // шрифтToolStripMenuItem
+            // 
+            this.шрифтToolStripMenuItem.Name = "шрифтToolStripMenuItem";
+            this.шрифтToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.шрифтToolStripMenuItem.Text = "Шрифт";
+            this.шрифтToolStripMenuItem.Click += new System.EventHandler(this.шрифтToolStripMenuItem_Click);
             // 
             // rtxtDoc
             // 
@@ -141,37 +155,6 @@
             this.rtxtDoc.Size = new System.Drawing.Size(388, 582);
             this.rtxtDoc.TabIndex = 1;
             this.rtxtDoc.Text = "";
-            // 
-            // toolStripMenuItem2
-            // 
-            this.toolStripMenuItem2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
-            this.toolStripMenuItem2.Text = "<-";
-            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
-            // 
-            // toolStripMenuItem3
-            // 
-            this.toolStripMenuItem3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
-            this.toolStripMenuItem3.Size = new System.Drawing.Size(152, 22);
-            this.toolStripMenuItem3.Text = "<- ->";
-            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
-            // 
-            // toolStripMenuItem4
-            // 
-            this.toolStripMenuItem4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
-            this.toolStripMenuItem4.Size = new System.Drawing.Size(152, 22);
-            this.toolStripMenuItem4.Text = "->";
-            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
-            // 
-            // шрифтToolStripMenuItem
-            // 
-            this.шрифтToolStripMenuItem.Name = "шрифтToolStripMenuItem";
-            this.шрифтToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
-            this.шрифтToolStripMenuItem.Text = "Шрифт";
-            this.шрифтToolStripMenuItem.Click += new System.EventHandler(this.шрифтToolStripMenuItem_Click);
             // 
             // frmDocument
             // 
@@ -200,8 +183,6 @@
         private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem печатьToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem видToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem шаблоныToolStripMenuItem;
         private System.Windows.Forms.RichTextBox rtxtDoc;
         private System.Windows.Forms.SaveFileDialog saveFileDialog;
         private System.Windows.Forms.ToolStripMenuItem форматированиеТекстаToolStripMenuItem;
--- Bilds_Fiz/Bilds_Fiz/Main.cs
+++ Bilds_Fiz/Bilds_Fiz/Main.cs
@@ -6,14 +6,67 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Bilds_Fiz
 {
+
     public partial class frmMain : Form
     {
+        
         public frmMain()
         {
             InitializeComponent();
+        }
+
+
+
+        public void addBuilds(int[] arrayProperty, List<string> listProperty, DateTime eventDate, bool light, bool water, bool hot)
+        {
+            string ev = eventDate.ToShortDateString();
+            eventDate = DateTime.Parse(ev);
+
+            dgvOne.DataSource = null;
+            //Build
+            TA_build.Insert(arrayProperty[0], arrayProperty[1].ToString(), listProperty[0], listProperty[1], eventDate);
+            TA_build.Fill(DS_Build_Fiz.Build);
+            dgvOne.DataSource = BS_build;
+            //About_S            
+            TA_buildAboutS.Insert(arrayProperty[0], arrayProperty[2], arrayProperty[3], arrayProperty[4], arrayProperty[5], arrayProperty[6], arrayProperty[7]);
+            TA_buildAboutS.Fill(DS_Build_Fiz.Build_About_S);
+            dgvOne.DataSource = BS_buildAboutS;            
+            //About
+            TA_buildAbout.Insert(arrayProperty[0], arrayProperty[8], light, water, hot, listProperty[2]);
+            TA_buildAbout.Fill(DS_Build_Fiz.Build_About);
+            dgvOne.DataSource = BS_buildAbout;
+            
+
+            TA_buildAboutS.Update(DS_Build_Fiz.Build_About_S);
+            TA_buildAbout.Update(DS_Build_Fiz.Build_About);
+            TA_build.Update(DS_Build_Fiz.Build);
+
+        }
+
+
+
+        public void addLiters(bool myself, int[] arrayProperty, List<string> listProperty)
+        {
+            
+            dgvOne.DataSource = null;
+            //Build-Liter
+            TA_build_Liter.Insert(arrayProperty[0], arrayProperty[12]);
+            TA_build_Liter.Fill(DS_Build_Fiz.Build_Liter);
+            dgvOne.DataSource = BS_buildLiter;
+            //Liter
+            byte type = byte.Parse(arrayProperty[1].ToString());
+            TA_liter.Insert(arrayProperty[0], listProperty[0], type, myself, arrayProperty[2], arrayProperty[3], arrayProperty[4], arrayProperty[5], listProperty[1], arrayProperty[6], arrayProperty[7], arrayProperty[8]
+                , listProperty[2], arrayProperty[9], arrayProperty[10], arrayProperty[11]);
+            TA_liter.Fill(DS_Build_Fiz.Liter);
+            dgvOne.DataSource = BS_liter;
+
+            TA_build_Liter.Update(DS_Build_Fiz.Build_Liter);
+            TA_liter.Update(DS_Build_Fiz.Liter);
+
         }
 
         private int OpenView(int n)
@@ -27,15 +80,11 @@ namespace Bilds_Fiz
 
         private void frmMain_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'DS_Build_Fiz.Liter' table. You can move, or remove it, as needed.
-            this.TA_liter.Fill(this.DS_Build_Fiz.Liter);
-            // TODO: This line of code loads data into the 'DS_Build_Fiz.Build_About_S' table. You can move, or remove it, as needed.
-            this.TA_buildAboutS.Fill(this.DS_Build_Fiz.Build_About_S);
-            // TODO: This line of code loads data into the 'DS_Build_Fiz.Build_About' table. You can move, or remove it, as needed.
-            this.TA_buildAbout.Fill(this.DS_Build_Fiz.Build_About);
-            // TODO: This line of code loads data into the 'dS_Build_Fiz.Liter' table. You can move, or remove it, as needed.
-            // TODO: This line of code loads data into the 'dS_Build_Fiz.Build' table. You can move, or remove it, as needed.
-            this.TA_build.Fill(this.DS_Build_Fiz.Build);
+            // TODO: This line of code loads data into the 'DS_Build_Fiz.Build_Liter' table. You can move, or remove it, as needed.
+            //this.TA_build_Liter.Fill(this.DS_Build_Fiz.Build_Liter);
+            // TODO: This line of code loads data into the 'DS_Build_Fiz.BuildAllInfo' table. You can move, or remove it, as needed.
+            //this.TA_buildAllInfo.Fill(this.DS_Build_Fiz.BuildAllInfo);
+
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -66,7 +115,7 @@ namespace Bilds_Fiz
         {
             try
             {
-                this.TA_build.FillBuild(this.DS_Build_Fiz.Build);
+                this.TA_build.Fill(this.DS_Build_Fiz.Build);
             }
             catch (System.Exception ex)
             {
@@ -75,66 +124,28 @@ namespace Bilds_Fiz
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            byte a = 10;
-            int aa = 2;
-            string b = "dfd";
-            string c = "trtrtrtr";
-            string d = "oii";
-            DateTime dt = new DateTime(2013, 2, 10);
-
-
-
-            //About_S
-            TA_buildAboutS.Insert(a, a, a, a, a, a, a);
-            dgvOne.DataSource = null;
-            TA_buildAboutS.Fill(DS_Build_Fiz.Build_About_S);
-            dgvOne.DataSource = BS_buildAboutS;
-            //About
-            TA_buildAbout.Insert(a, aa, true, true, true, c);
-            dgvOne.DataSource = null;
-            TA_buildAbout.Fill(DS_Build_Fiz.Build_About);
-            dgvOne.DataSource = BS_buildAbout;
-            //Build
-            TA_build.Insert(a, b, c, d, dt);
-            dgvOne.DataSource = null;
-            TA_build.Fill(DS_Build_Fiz.Build);
-            dgvOne.DataSource = BS_build;
-
-        }
-
         private int picPic(int n)
         {
-            switch (n)
-            {
-                case 1:
-                    pictureBox1.Image = Properties.Resources._2;
-                    pictureBox2.Image = Properties.Resources._1;
-                    pictureBox3.Image = Properties.Resources._1;
-                    pictureBox4.Image = Properties.Resources._1;
-                    break;
-                case 2:
-                    pictureBox1.Image = Properties.Resources._1;
-                    pictureBox2.Image = Properties.Resources._2;
-                    pictureBox3.Image = Properties.Resources._1;
-                    pictureBox4.Image = Properties.Resources._1;
-                    break;
-                case 3:
-                    pictureBox1.Image = Properties.Resources._1;
-                    pictureBox2.Image = Properties.Resources._1;
-                    pictureBox3.Image = Properties.Resources._2;
-                    pictureBox4.Image = Properties.Resources._1;
-                    break;
-                case 4:
-                    pictureBox1.Image = Properties.Resources._1;
-                    pictureBox2.Image = Properties.Resources._1;
-                    pictureBox3.Image = Properties.Resources._1;
-                    pictureBox4.Image = Properties.Resources._2;
-                    break;
-            }
-            return 0;
-        }        
+           switch(n)
+           {
+               case 1: 
+                   pictureBox1.Image = Properties.Resources._1;
+                   pictureBox2.Image = Properties.Resources._1;
+                   pictureBox3.Image = Properties.Resources._2;
+            break;
+               case 2:
+            pictureBox1.Image = Properties.Resources._2;
+            pictureBox2.Image = Properties.Resources._1;
+            pictureBox3.Image = Properties.Resources._1;
+            break;
+               case 3:
+            pictureBox1.Image = Properties.Resources._1;
+            pictureBox2.Image = Properties.Resources._2;
+            pictureBox3.Image = Properties.Resources._1;
+            break;
+        }
+           return 0;
+        }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
@@ -142,7 +153,7 @@ namespace Bilds_Fiz
             dgvOne.DataSource = DS_Build_Fiz.Build;
             dgvOne.Update();
 
-            picPic(1);
+            picPic(2);
 
         }
 
@@ -152,7 +163,7 @@ namespace Bilds_Fiz
             dgvOne.DataSource = DS_Build_Fiz.Build_About;
             dgvOne.Update();
 
-            picPic(2);
+                    
         }
 
         private void pictureBox3_Click(object sender, EventArgs e)
@@ -161,7 +172,7 @@ namespace Bilds_Fiz
             dgvOne.DataSource = DS_Build_Fiz.Build_About_S;
             dgvOne.Update();
 
-            picPic(3);
+           
         }
 
         private void pictureBox4_Click(object sender, EventArgs e)
@@ -170,7 +181,7 @@ namespace Bilds_Fiz
             dgvOne.DataSource = DS_Build_Fiz.Liter;
             dgvOne.Update();
 
-            picPic(4);
+            picPic(3);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -205,7 +216,154 @@ namespace Bilds_Fiz
             frm.ShowDialog();
         }
 
+        private void label5_Click(object sender, EventArgs e)
+        {
+            OpenView(5);
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            TA_buildAllInfo.Fill(DS_Build_Fiz.BuildAllInfo);
+            dgvOne.DataSource = DS_Build_Fiz.BuildAllInfo;
+            dgvOne.Update();
+
+            picPic(1);
+        }
+
+        private void вДокументToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                string table = dgvOne.DataSource.ToString();
+                frmDocument frm = new frmDocument();
+                List<string> str = new List<string>();
+
+                switch (table)
+                {
+                    case "Build":
+                        docBuildShot(frm, str);
+                        break;
+                    case "Liter":
+                        docLiter(frm, str);
+                        break;
+                    case "BuildAllInfo":
+                        docBuildAllInfo(frm, str);
+                        break;
+                }
+                frm.Show();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не открыта таблица", "Ошибка чтения данных из таблицы", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+            }
+                
+          
+        }
+
+        private void docBuildAllInfo(frmDocument frm, List<string> str)
+        {
+            DateTime date = new DateTime();
+            date = DateTime.Parse(dgvOne.CurrentRow.Cells[4].Value.ToString());
+
+            str.Add("BuildAllInfo");
+            str.Add(dgvOne.CurrentRow.Cells[0].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[1].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[2].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[3].Value.ToString());
+            str.Add(date.ToShortDateString());
+            str.Add(dgvOne.CurrentRow.Cells[5].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[6].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[7].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[8].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[9].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[10].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[11].Value.ToString());
+            if (bool.Parse(dgvOne.CurrentRow.Cells[12].Value.ToString()) == true)
+            {
+                str.Add("Есть");
+            }
+            else
+            {
+                str.Add("Отсутствует");
+            }
+            if (bool.Parse(dgvOne.CurrentRow.Cells[13].Value.ToString()) == true)
+            {
+                str.Add("Есть");
+            }
+            else
+            {
+                str.Add("Отсутствует");
+            }
+            if(bool.Parse(dgvOne.CurrentRow.Cells[14].Value.ToString()) == true)
+            {
+                str.Add("Есть");
+            }
+            else
+            {
+                str.Add("Отсутствует");
+            }
+
+            str.Add(dgvOne.CurrentRow.Cells[15].Value.ToString());            
+            
+            frm.doc(str);
+        }
+
+        private void docLiter(frmDocument frm, List<string> str)
+        {
+            str.Add("Liter");
+            str.Add(dgvOne.CurrentRow.Cells[0].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[1].Value.ToString());
+
+            if (dgvOne.CurrentRow.Cells[2].Value.ToString() == "1")
+            {
+                str.Add("Основной");
+            }
+            else
+            {
+                str.Add("Дополнительный");
+            }
+
+            str.Add(dgvOne.CurrentRow.Cells[3].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[4].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[5].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[6].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[7].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[8].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[9].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[10].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[11].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[12].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[13].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[14].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[15].Value.ToString());
+
+            frm.doc(str);
+         
+        }
+
+        private void docBuildShot(frmDocument frm, List<string> str)
+        {
+            DateTime date = DateTime.Parse(dgvOne.CurrentRow.Cells[4].Value.ToString());
+            
+            str.Add("Build");
+            str.Add(dgvOne.CurrentRow.Cells[0].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[1].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[2].Value.ToString());
+            str.Add(dgvOne.CurrentRow.Cells[3].Value.ToString());
+            str.Add(date.ToShortDateString());
+
+            frm.doc(str);  
+        }
+
       
-      
+
+        //Обновление таблицы
+
+        //dgvOne.DataSource = null;
+        //TA_liter.Fill(DS_Build_Fiz.Liter);
+        //dgvOne.DataSource = BS_liter;
+
     }
 }
